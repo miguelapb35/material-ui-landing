b197b15a5b256ce89e40d3852e1f12ed57bf8204
Miguel Angel <miguelapb35@gmail.com>
2020-10-16 21:07:06 -0500

    Updates

---
+++ precache-manifest.48fae8105fc0c0a8a380f1b169624748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1424ad4b83b79208416de28d3facdf7f",
+    "url": "/material-ui-landing/index.html"
+  },
+  {
+    "revision": "a000fa9f9774e4cbceb6",
+    "url": "/material-ui-landing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4372e56bbb53526e07b7",
+    "url": "/material-ui-landing/static/js/2.1dc95ffc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/material-ui-landing/static/js/2.1dc95ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a000fa9f9774e4cbceb6",
+    "url": "/material-ui-landing/static/js/main.1dad7ab7.chunk.js"
+  },
+  {
+    "revision": "1001a021788adba54af3",
+    "url": "/material-ui-landing/static/js/runtime-main.332de343.js"
+  }
+]);
